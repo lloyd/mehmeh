7723a7ccd4041326674e2eaa9681be91df0ccd12
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-12-04 15:46:45 -0700

    make it guten

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 foo: sha256.c sha256.h main.c sysendian.h
-	gcc -g -o doit sha256.c main.c
+	gcc -O3 -o doit sha256.c main.c
--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include "sha256.h"
+#include <string.h>
+#include <stdio.h>
 
 int main()
 {
-    static unsigned char passwd[] = { 'p', 'a', 's', 's', 'w', 'o', 'r', 'd' };
-    static unsigned char salt[] =
-        "identity.mozilla.com/gombot/v1/derivation:foo@example.org";
-    static unsigned char saltAuth[] =
-        { 'i', 'd', 'e', 'n', 't', 'i', 't', 'y', '.', 'm', 'o', 'z', 'i', 'l', 'l', 'a', '.', 'c', 'o', 'm', '/', 'g', 'o', 'm', 'b', 'o', 't', '/', 'v', '1', '/', 'a', 'u', 't', 'h', 'e', 'n', 't', 'i', 'c', 'a', 't', 'i', 'o', 'n',  };
-    static unsigned char saltCrypt[] =
-        { 'i', 'd', 'e', 'n', 't', 'i', 't', 'y', '.', 'm', 'o', 'z', 'i', 'l', 'l', 'a', '.', 'c', 'o', 'm', '/', 'g', 'o', 'm', 'b', 'o', 't', '/', 'v', '1', '/', 'e', 'n', 'c', 'r', 'y', 'p', 't', 'i', 'o', 'n',  };
+    static unsigned const char * passwd = "password";
+    static unsigned const char * salt = "identity.mozilla.com/gombot/v1/derivation:foo@example.org";
+    static unsigned const char * saltAuth = "identity.mozilla.com/gombot/v1/authentication";
+    static unsigned const char * saltCrypt = "identity.mozilla.com/gombot/v1/encryption";
 
     unsigned char dkey[32];
     unsigned char akey[32];
     unsigned char ckey[32];
     int i;
 
-    s_PBKDF2_SHA256(passwd, sizeof(passwd), salt, sizeof(salt), 250000, dkey, sizeof(dkey));
-    s_PBKDF2_SHA256(dkey, sizeof(dkey), saltAuth, sizeof(saltAuth), 1, akey, sizeof(akey));
-    s_PBKDF2_SHA256(dkey, sizeof(dkey), saltCrypt, sizeof(saltCrypt), 1, ckey, sizeof(ckey));
+    s_PBKDF2_SHA256(passwd, strlen(passwd), salt, strlen(salt), 250000, dkey, sizeof(dkey));
+    s_PBKDF2_SHA256(dkey, sizeof(dkey), saltAuth, strlen(saltAuth), 1, akey, sizeof(akey));
+    s_PBKDF2_SHA256(dkey, sizeof(dkey), saltCrypt, strlen(saltCrypt), 1, ckey, sizeof(ckey));
 
     printf("authKey: ");
     for (i=0; i<sizeof(akey); i++) {
